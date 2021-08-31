7fff7d667366125bd457faee92d5dfd0732d09e9
Stormboys3 <89860909+Stormboys3@users.noreply.github.com>
2021-08-31 11:15:42 -0500

    Update FollowPlayer.cs

--- FollowPlayer.cs
+++ FollowPlayer.cs
@@ -8,16 +8,10 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void LateUpdate()
     {
-        //Follows the vehicle
+        //Makes a gameobject follow player
         transform.position = player.transform.position + offset;
     }
 }
